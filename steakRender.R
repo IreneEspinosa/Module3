5a72bbfd5ade806d30eed1e9d250c21e870db886
Irene Espinosa <irenee70@gmail.com>
2018-11-14 14:39:29 +0100

    new script for steakArticle

--- steakRender.R
+++ steakRender.R
@@ -14,3 +14,17 @@ render_report <- function(regionvar) {
 }
 
 render_report("Pacific")
+
+library(purrr)
+params_list <- list(list("East North Central" ,
+                         "South Atlantic" , "New England" ,
+                         "Middle Atlantic" , "West South Central" ,
+                         "West North Central" , "Pacific" ,           
+                         "Mountain" , "East South Central" ))
+purrr::pmap(params_list, render_report)
+
+
+
+
+
+
